44af876d6d7f20b7065a7ce751e75a64bfc34fed
unknown <solimannb@gmail.com>
2020-10-29 23:58:28 +0200

    change menu route , add custom css js code

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/',function (){
+    return view('frontsite.home');
+})->name('frontsite.home');
+
+Route::get('single',function (){
+    return view('frontsite.single');
+})->name('frontsite.single');
+
+Route::get('blog',function (){
+    return view('frontsite.blog');
+})->name('frontsite.blog');
+
+Route::get('contact',function (){
+    return view('frontsite.contact');
+})->name('frontsite.contact');
